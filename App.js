7461851d4f6910f6fa8908f4b16d5ed65e3bfe3a
Mona Lisa <longle2243@gmail.com>
2022-02-19 10:23:56 +0700

    code chua fix

---
+++ App.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { Text, View,Button} from 'react-native';
+import { styles } from "./stylesheet";
+
+import { useNavigation, NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import 'react-native-gesture-handler';
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+function GoToScreen({ screenName }) {
+  const navigation = useNavigation();
+  return (
+    <Button 
+      title={`Go to ${screenName}`}
+      onPress={() => navigation.navigate(screenName)}
+    />
+  );
+}
+function SanPham({ navigation }) {
+  return (
+    <View>
+      <View style={styles.container}>
+        <View style={{display: 'none'}}>
+        <GoToScreen screenName="Home" />
+        </View> 
+    </View>
+    {/* <BottomTabs/> */}
+    </View>
+    
+  );
+}
+function TaiKhoan({ navigation }) {
+  return (
+    <View style={styles.container}>
+        {/* <Text style={styles.text} onPress={()=>{navigation.navigate("HomeScreen")}}>Day la trang chi tiet San Pham</Text> */}
+        <GoToButton screenName="CaNhan" />
+    </View>
+  );
+}
+function Home({navigation}) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.box}>
+        <View style={styles.inner}>
+          <GoToScreen screenName="SanPham" />
+        </View>
+      </View>
+    </View> 
+  );
+}
+function GioHang() {
+  return (
+    <View>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Gio Hang</Text>
+    </View>
+    <Drawers/>
+    </View>
+  );
+}
+function CaNhan() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Ca Nhan</Text>
+    </View>
+  );
+}
+function BottomTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="HomeTabs" component={Drawers} options={{ headerShown: false }}/>
+      <Tab.Screen name="Gio Hang" component={GioHang} />
+      <Tab.Screen name="Ca Nhan" component={CaNhan} />
+    </Tab.Navigator>
+    
+  );
+}
+function Drawers() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Homedawer" component={Home} />
+      <Drawer.Screen name="TaiKhoan" component={TaiKhoan} />
+    </Drawer.Navigator>
+  );
+}
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator >
+        <Stack.Screen name="HomeStack" component={BottomTabs} options={{ headerShown: false }}/>
+        <Stack.Screen name="SanPham" component={SanPham} />
+        <Stack.Screen name="Drawers" component={Drawers} options={{ headerShown: false }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ stylesheet.js
@@ -0,0 +1,70 @@
+import { StyleSheet } from 'react-native';
+export const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '85%',
+    padding: 5,
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  box: {
+    width: '50%',
+    height: '50%',
+    padding: 5
+  },
+  inner: {
+    flex: 1,
+    // backgroundColor: '#D1E0DB',
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: "#33CCFF",
+    fontSize: 30,
+  },
+  containerlogin: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: '60%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: '700',
+    fontSize: 16,
+  },
+});
